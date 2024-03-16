d1de64570ea5e0b35f46567ae58bfcbc39137040
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-03-16 10:06:22 +0100

    update blogCard

---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,33 @@
+import styles from './BlogCard.module.css';
+import { formatDate } from 'helpers/formatDate';
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  userName,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <img className={styles.cardPoster} src={poster} alt={description} />
+      </div>
+      <div className={styles.cardBody}>
+        <span className={styles.tag}>{tag}</span>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p className={styles.cardText}>{description}</p>
+      </div>
+      <div className={styles.cardFooter}>
+        <div className={styles.userBox}>
+          <img className={styles.avatar} src={avatar} alt={userName} />
+          <div>
+            <h3 className={styles.userName}>{userName}</h3>
+            <small className={styles.date}>{formatDate(postedAt)}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
