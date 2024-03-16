1320db829b5d55d2f626105b630b2c8cd82f40e5
AntoninaMed <ton.kraski2427@gmail.com>
2024-03-16 12:35:14 +0200

    commit

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,28 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import { ForbesListItem } from 'components/ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+export const ForbesList = ({ list }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map(({ id, name, capital, avatar, isIncrease }) => (
+          <li className={style.item}  key={id}>
+            <ForbesListItem
+              avatar={avatar}
+              capital={capital}
+              name={name}
+              isIncrease={isIncrease}
+             
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
